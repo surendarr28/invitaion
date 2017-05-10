8158c7d4675c9f1b8f215fd0dbdf77714dce9d32
surendar <surendarr@ihorsetechnologies.com>
2017-05-10 18:11:56 +0530

    [Port Update]

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
     console.log('Example app listening on port 3000!')
 })
